88e49d0783be5d0b055c10ef61242634eeea4d3e
Marco Palacios <mapalacios2@espe.edu.ec>
2016-02-22 17:41:13 -0500

    Correccion jRadioBox

--- src/main/java/com/teamj/distribuidas/aplicacioncajero/ui/InterfazPrincipal.java
+++ src/main/java/com/teamj/distribuidas/aplicacioncajero/ui/InterfazPrincipal.java
@@ -48,9 +48,7 @@ public class InterfazPrincipal extends javax.swing.JFrame {
         jLabel8 = new javax.swing.JLabel();
         txtMonto = new javax.swing.JTextField();
         jLabel9 = new javax.swing.JLabel();
-        jRadioButton1 = new javax.swing.JRadioButton();
         jLabel3 = new javax.swing.JLabel();
-        jRadioButton2 = new javax.swing.JRadioButton();
         jLabel10 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
@@ -58,6 +56,8 @@ public class InterfazPrincipal extends javax.swing.JFrame {
         lblSaldo = new javax.swing.JLabel();
         jLabel12 = new javax.swing.JLabel();
         jComboBox1 = new javax.swing.JComboBox<>();
+        rbnDeposito = new javax.swing.JRadioButton();
+        rbnRetiro = new javax.swing.JRadioButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -91,21 +91,7 @@ public class InterfazPrincipal extends javax.swing.JFrame {
 
         jLabel9.setText("MONTO:");
 
-        jRadioButton1.setText("Depósito");
-        jRadioButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jRadioButton1ActionPerformed(evt);
-            }
-        });
-
         jLabel3.setText("TIPO:");
-
-        jRadioButton2.setText("Retiro");
-        jRadioButton2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jRadioButton2ActionPerformed(evt);
-            }
-        });
 
         jLabel10.setText("REGISTRAR MOVIMIENTO");
 
@@ -131,6 +117,20 @@ public class InterfazPrincipal extends javax.swing.JFrame {
 
         jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "AHORRO", "CORRIENTE" }));
 
+        rbnDeposito.setText("Deposito");
+        rbnDeposito.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                rbnDepositoActionPerformed(evt);
+            }
+        });
+
+        rbnRetiro.setText("Retiro");
+        rbnRetiro.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                rbnRetiroActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -141,43 +141,44 @@ public class InterfazPrincipal extends javax.swing.JFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jLabel10)
+                            .addComponent(jLabel1)
+                            .addComponent(jLabel8)
+                            .addComponent(jLabel4)
+                            .addComponent(jLabel7)
                             .addGroup(layout.createSequentialGroup()
-                                .addGap(27, 27, 27)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                                    .addComponent(jButton2)
-                                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                        .addComponent(jLabel3)
-                                        .addComponent(jLabel9)))
-                                .addGap(44, 44, 44)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                    .addComponent(jRadioButton2)
-                                    .addComponent(jRadioButton1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                    .addComponent(txtMonto, javax.swing.GroupLayout.Alignment.TRAILING))))
-                        .addGap(21, 21, 21)
-                        .addComponent(jButton1))
-                    .addComponent(jLabel1)
-                    .addComponent(jLabel8)
-                    .addComponent(jLabel4)
-                    .addComponent(jLabel7)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel11)
-                        .addGap(25, 25, 25)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(lblSaldo)
-                            .addComponent(lblCedula)
-                            .addComponent(lblCliente)))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel2)
-                            .addComponent(jLabel12))
-                        .addGap(39, 39, 39)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(jLabel11)
+                                .addGap(25, 25, 25)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(lblSaldo)
+                                    .addComponent(lblCedula)
+                                    .addComponent(lblCliente)))
                             .addGroup(layout.createSequentialGroup()
-                                .addComponent(txtdatos, javax.swing.GroupLayout.PREFERRED_SIZE, 97, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(45, 45, 45)
-                                .addComponent(Btnbuscar)))))
-                .addContainerGap(26, Short.MAX_VALUE))
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jLabel2)
+                                    .addComponent(jLabel12))
+                                .addGap(39, 39, 39)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addComponent(txtdatos, javax.swing.GroupLayout.PREFERRED_SIZE, 97, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addGap(45, 45, 45)
+                                        .addComponent(Btnbuscar)))))
+                        .addContainerGap(26, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(27, 27, 27)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jButton2)
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addComponent(jLabel3)
+                                .addComponent(jLabel9)))
+                        .addGap(44, 44, 44)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(txtMonto, javax.swing.GroupLayout.PREFERRED_SIZE, 67, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(rbnDeposito)
+                            .addComponent(rbnRetiro))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jButton1)
+                        .addGap(56, 56, 56))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -214,12 +215,12 @@ public class InterfazPrincipal extends javax.swing.JFrame {
                     .addComponent(jLabel9)
                     .addComponent(txtMonto, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(24, 24, 24)
-                .addComponent(jLabel3)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jRadioButton1)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel3)
+                    .addComponent(rbnDeposito))
                 .addGap(18, 18, 18)
-                .addComponent(jRadioButton2)
-                .addGap(18, 18, 18)
+                .addComponent(rbnRetiro)
+                .addGap(34, 34, 34)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButton1)
                     .addComponent(jButton2))
@@ -260,15 +261,6 @@ public class InterfazPrincipal extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_txtMontoActionPerformed
 
-    private void jRadioButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jRadioButton1ActionPerformed
-        // TODO add your handling code here:
-
-    }//GEN-LAST:event_jRadioButton1ActionPerformed
-
-    private void jRadioButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jRadioButton2ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jRadioButton2ActionPerformed
-
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
         this.dispose();
@@ -276,14 +268,17 @@ public class InterfazPrincipal extends javax.swing.JFrame {
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
-        String tipo;
+        String tipo="";
         String tipoC;
-        if (jRadioButton1.isSelected()) {
+        if (rbnDeposito.isSelected()) {
             tipo = "DE";
 
-        } else {
+        } else if(rbnRetiro.isSelected()) {
             tipo = "RE";
+        } else{
+            JOptionPane.showMessageDialog(null,"Elija el tipo de transaccion");
         }
+            
         if (jComboBox1.getSelectedItem() == "AHORRO") {
             tipoC = "AH";
 
@@ -293,8 +288,8 @@ public class InterfazPrincipal extends javax.swing.JFrame {
         double monto = new Double(txtMonto.getText());
         double saldo = new Double(lblSaldo.getText());
 
-        if (monto > saldo && tipo.equals("RE")) {
-            JOptionPane.showMessageDialog(null, "El monto del retiro no puede superar el valor del saldo actual");
+        if (monto >= saldo && tipo.equals("RE")) {
+            JOptionPane.showMessageDialog(null, "El monto de retiro no puede superar el valor del saldo actual");
 
         } else if (!txtMonto.getText().isEmpty()) {
 
@@ -315,6 +310,17 @@ public class InterfazPrincipal extends javax.swing.JFrame {
             JOptionPane.showMessageDialog(null, "Ingrese la cantidad primero");
         }
     }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void rbnRetiroActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_rbnRetiroActionPerformed
+           // TODO add your handling code here:
+           rbnDeposito.setSelected(false);
+    }//GEN-LAST:event_rbnRetiroActionPerformed
+
+    private void rbnDepositoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_rbnDepositoActionPerformed
+         // TODO add your handling code here:
+          rbnRetiro.setSelected(false);
+        
+    }//GEN-LAST:event_rbnDepositoActionPerformed
 
     /**
      * @param args the command line arguments
@@ -399,11 +405,11 @@ public class InterfazPrincipal extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel8;
     private javax.swing.JLabel jLabel9;
-    private javax.swing.JRadioButton jRadioButton1;
-    private javax.swing.JRadioButton jRadioButton2;
     public javax.swing.JLabel lblCedula;
     public javax.swing.JLabel lblCliente;
     public javax.swing.JLabel lblSaldo;
+    private javax.swing.JRadioButton rbnDeposito;
+    private javax.swing.JRadioButton rbnRetiro;
     private javax.swing.JTextField txtMonto;
     private javax.swing.JTextField txtdatos;
     // End of variables declaration//GEN-END:variables
